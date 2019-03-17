6daa70c8cec554799151cc783ab7567189a968c9
wangerxiao <863579016@qq.com>
2019-03-17 18:03:36 +0800

    test proto buf

--- src/main/java/com/yao/TryWith.java
+++ src/main/java/com/yao/TryWith.java
@@ -3,12 +3,18 @@ package com.yao;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Random;
+import java.util.UUID;
 
 public class TryWith {
 
     public static void main(String[]args) throws IOException {
 
-        BufferedReader reader = new BufferedReader(new FileReader("/Users/robin/Workspace/demo/src/main/java/com/yao/Hello.java"));
+
+
+        System.out.println(UUID.randomUUID());
+        System.out.println(new Random().nextInt(1000000));
+        //BufferedReader reader = new BufferedReader(new FileReader("/Users/robin/Workspace/demo/src/main/java/com/yao/Hello.java"));
 
         //try(reader){
         //    String line = null;
--- src/main/java/com/yao/serialization/TestProtoBuf.java
+++ src/main/java/com/yao/serialization/TestProtoBuf.java
@@ -1,4 +1,24 @@
 package com.yao.serialization;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+
 public class TestProtoBuf {
+
+    public static void main(String[]args) throws InvalidProtocolBufferException {
+
+        PersonProtos.Person.Builder personBuilder =  PersonProtos.Person.newBuilder();
+        personBuilder.setId(111);
+        personBuilder.setEmail("xx@xx");
+        personBuilder.setName("wanger");
+
+        PersonProtos.Person person = personBuilder.build();
+
+        byte[] bytes = person.toByteArray();
+        PersonProtos.Person result = PersonProtos.Person.parseFrom(bytes);
+        System.out.println(result.getId());
+        System.out.println(result.getEmail());
+        System.out.println(result.getName());
+
+    }
+
 }
